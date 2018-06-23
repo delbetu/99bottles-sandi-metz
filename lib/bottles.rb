debe3437cc395b3252c44061bfe710759d159546
Marcos Bellucci <mbellucci@salesforce.com>
2018-06-23 09:38:30 -0300

    Start with simplest method and parameter verse(99)
    Write simplest code to satisfy error until it passes.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,9 @@
 class Bottles
   def verse(n)
+    "99 bottles of beer on the wall," +
+    "99 bottles of beer,\n" +
+    "Take one down and pass it around," +
+    "98 bottles of beer on the wall.\n"
   end
 
   def verses(a, b)
