17134fbcb92ed5952682979a9521281b222525a6
Marcos Bellucci <mbellucci@salesforce.com>
2018-06-23 09:50:23 -0300

    Write three possible solutions.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,44 @@
 class Bottles
   def verse(n)
-    "99 bottles of beer on the wall," +
-    "99 bottles of beer,\n" +
+    # conditional(n)
+    # sparsed_conditional(n)
+    # interpolation(n)
+  end
+
+  def conditional(n)
+    # (unconditional->if)
+    if n == 99 # (nil->constant) new integer 99
+      "99 bottles of beer on the wall," +
+      "99 bottles of beer,\n" +
+      "Take one down and pass it around," +
+      "98 bottles of beer on the wall.\n"
+    else
+      "3 bottles of beer on the wall," + # (nil->constant) new string constant
+      "3 bottles of beer,\n" +
+      "Take one down and pass it around," +
+      "2 bottles of beer on the wall.\n"
+    end
+  end
+
+  def sparsed_conditional(n)
+    # (unconditional->if)
+    if n == 99 # (nil->constant)
+      number = 99 # (nil->constant->scalar)
+    else
+      number = 3 # (nil->constant)
+    end
+
+    "#{number} bottles of beer on the wall," + # (constant->expression)
+    "#{number} bottles of beer,\n" +
     "Take one down and pass it around," +
-    "98 bottles of beer on the wall.\n"
+    "#{number-1} bottles of beer on the wall.\n"
+  end
+
+  def interpolation(n)
+    "#{n} bottles of beer on the wall," + # (constant->expression)
+    "#{n} bottles of beer,\n" +
+    "Take one down and pass it around," +
+    "#{n-1} bottles of beer on the wall.\n"
   end
 
   def verses(a, b)
