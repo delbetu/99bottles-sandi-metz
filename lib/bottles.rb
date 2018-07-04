f0a9874eaf3517138a23a370ef1f6780836158a1
Marcos Bellucci <mbellucci@salesforce.com>
2018-07-04 19:22:32 -0300

    Follow priority premise to choose simplest transformation.
    Transformation from simple to complex
      * ({}–>nil) no code at all->code that employs nil
      * (nil->constant)
      * (constant->constant+) a simple constant to a more complex constant
      * (constant->scalar) replacing a constant with a variable or an argument
      * (statement->statements) adding more unconditional statements.
      * (unconditional->if) splitting the execution path
      * (scalar->array)
      * (array->container)
      * (statement->recursion)
      * (if->while)
      * (expression->function) replacing an expression with a function or algorithm
      * (variable->assignment) replacing the value of a variable.
    
    Priority-Premise —> Simpler transformations should be applied first.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,41 +1,6 @@
 class Bottles
   def verse(n)
-    # conditional(n)
-    # sparsed_conditional(n)
-    # interpolation(n)
-  end
-
-  def conditional(n)
-    # (unconditional->if)
-    if n == 99 # (nil->constant) new integer 99
-      "99 bottles of beer on the wall," +
-      "99 bottles of beer,\n" +
-      "Take one down and pass it around," +
-      "98 bottles of beer on the wall.\n"
-    else
-      "3 bottles of beer on the wall," + # (nil->constant) new string constant
-      "3 bottles of beer,\n" +
-      "Take one down and pass it around," +
-      "2 bottles of beer on the wall.\n"
-    end
-  end
-
-  def sparsed_conditional(n)
-    # (unconditional->if)
-    if n == 99 # (nil->constant)
-      number = 99 # (nil->constant->scalar)
-    else
-      number = 3 # (nil->constant)
-    end
-
-    "#{number} bottles of beer on the wall," + # (constant->expression)
-    "#{number} bottles of beer,\n" +
-    "Take one down and pass it around," +
-    "#{number-1} bottles of beer on the wall.\n"
-  end
-
-  def interpolation(n)
-    "#{n} bottles of beer on the wall," + # (constant->expression)
+    "#{n} bottles of beer on the wall," +
     "#{n} bottles of beer,\n" +
     "Take one down and pass it around," +
     "#{n-1} bottles of beer on the wall.\n"
