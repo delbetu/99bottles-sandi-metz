a899147ce536f7634822ff0caad7de2d27301eda
Marcos Bellucci <mbellucci@salesforce.com>
2018-07-04 22:32:23 -0300

    Chose stark_conditional
    Interpolated is harder to understand.
    Both have same rank on priority premise.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,6 @@
 class Bottles
   def verse(n)
-    # both are the same transformation uncondition->conditional
-    # interpolated_conditional is harder to understand.
-    # stark_conditional
-    # interpolated_conditional
-  end
-
-  def stark_conditional(n)
-    if n == 2 # nil->constant unconditional->conditional, 
+    if n == 2
       "2 bottles of beer on the wall," + # nil->constant
       "2 bottles of beer,\n" +
       "Take one down and pass it around," +
@@ -18,13 +11,6 @@ class Bottles
       "Take one down and pass it around," +
       "#{n-1} bottles of beer on the wall.\n"
     end
-  end
-
-  def interpolated_conditional(n)
-    "#{n} bottles of beer on the wall," +
-    "#{n} bottles of beer,\n" +
-    "Take one down and pass it around," +
-    "#{n-1} bottle#{'s' unless n-1 == 1} of beer on the wall.\n" # unconditional->if
   end
 
   def verses(a, b)
