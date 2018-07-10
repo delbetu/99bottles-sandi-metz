ce3325856d4119039acdb7409a2c511d3417d66c
Marcos Bellucci <mbellucci@salesforce.com>
2018-07-10 11:11:46 -0300

    Delete branch 2
    branch-2 and branch-else are identical

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,11 +11,6 @@ class Bottles
       "1 bottle of beer,\n" +
       "Take it down and pass it around," +
       "no more bottles of beer on the wall.\n"
-    when 2
-      "#{n} bottles of beer on the wall," +
-      "#{n} bottles of beer,\n" +
-      "Take one down and pass it around," +
-      "#{n-1} #{container(n-1)} of beer on the wall.\n"
     else
       "#{n} bottles of beer on the wall," +
       "#{n} bottles of beer,\n" +
