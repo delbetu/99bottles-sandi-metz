4aa67afc561b103384efd7758e64c71ff70f183f
Marcos Bellucci <mbellucci@salesforce.com>
2018-07-10 10:29:44 -0300

    [smell-duplication] Flocking Rules 1
    Most alike branches 2 and else.
    Difference = bottle vs bottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,10 +12,10 @@ class Bottles
       "Take it down and pass it around," +
       "no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall," + # nil->constant
-      "2 bottles of beer,\n" +
+      "#{n} bottles of beer on the wall," +
+      "#{n} bottles of beer,\n" +
       "Take one down and pass it around," +
-      "1 bottle of beer on the wall.\n"
+      "#{n-1} bottle of beer on the wall.\n"
     else
       "#{n} bottles of beer on the wall," +
       "#{n} bottles of beer,\n" +
