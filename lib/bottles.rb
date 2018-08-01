8dc1ec4014e383ab668596261286edf10bfca9bf
Marcos Bellucci <mbellucci@salesforce.com>
2018-08-01 11:01:58 -0300

    Remove data clump

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,10 +11,10 @@ class Bottles
     bottle_number = BottleNumber.new(n)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall," +
-    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number}".capitalize + " of beer on the wall," +
+    "#{bottle_number} of beer.\n" +
     bottle_number.action +
-    "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+    "#{next_bottle_number} of beer on the wall.\n"
   end
 end
 
